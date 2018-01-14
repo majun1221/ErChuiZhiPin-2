e8db3b60b7945732690d33747da99ccce028459d
马俊 <836046371@qq.com>
2018-01-14 09:05:39 +0800

    Initial Commit

---
+++ ErChuiZhiPin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ErChuiZhiPin
+//
+//  Created by 陕西耳垂网络科技有限公司 on 2018/1/14.
+//  Copyright © 2018年 陕西耳垂网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ErChuiZhiPin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ErChuiZhiPin
+//
+//  Created by 陕西耳垂网络科技有限公司 on 2018/1/14.
+//  Copyright © 2018年 陕西耳垂网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
